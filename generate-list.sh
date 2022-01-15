07bc7bf3d818481405893cdf20c326ef2673ad7f
Natan Lao <natanlao@users.noreply.github.com>
2022-01-14 23:54:48 -0800

    Add list header

--- generate-list.sh
+++ generate-list.sh
@@ -3,8 +3,16 @@
 # TODO: strict
 # TODO: preamble
 
+echo "! Title: Annoying search results"
+echo "! Version: $(date +%Y%M%d%H%M)"
+echo "! Last modified: $(date)"
+echo "! Homepage: https://github.com/natanlao/ublock-origin-search-annoyance-list"
+echo "! License: https://github.com/natanlao/ublock-origin-search-annoyance-list/blob/develop/LICENSE"
+
+
 for list in lists/*.list.txt; do
     for domain in $(cat "$list"); do
+        # Block each specified domain (and all subdomains) on Google and DuckDuckGo 
         echo "duckduckgo.*##.results > div:has(a[href*=\"$domain\"])"
         echo "duckduckgo.*##.results > div:has(a[href*=\"*.$domain\"])"
         echo "google.*##.g:has(a[href*=\"$domain\"])"
