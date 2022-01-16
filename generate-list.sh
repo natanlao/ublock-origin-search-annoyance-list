87f49bf5bbc76401c4c0b709f7075da5c04204ea
Natan Lao <natanlao@users.noreply.github.com>
2022-01-15 22:38:38 -0800

    Handle full URLs properly

--- generate-list.sh
+++ generate-list.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
-
-# TODO: strict
-# TODO: preamble
+set -eEu -o pipefail
 
 echo "! Title: Annoying search results"
 echo "! Version: $(date +%Y%M%d%H%M)"
@@ -14,9 +12,11 @@ for list in lists/*.list.txt; do
     for domain in $(cat "$list"); do
         # Block each specified domain (and all subdomains) on Google and DuckDuckGo 
         echo "duckduckgo.*##.results > div:has(a[href*=\"$domain\"])"
-        echo "duckduckgo.*##.results > div:has(a[href*=\"*.$domain\"])"
         echo "google.*##.g:has(a[href*=\"$domain\"])"
-        echo "google.*##.g:has(a[href*=\"*.$domain\"])"
+        if [[ $domain != https://* ]]; then
+            echo "duckduckgo.*##.results > div:has(a[href*=\"*.$domain\"])"
+            echo "google.*##.g:has(a[href*=\"*.$domain\"])"
+        fi
     done
 done
 echo
